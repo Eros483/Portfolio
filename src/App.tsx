5c70a4601834ac2b40d410bee701904560ff5560
Eros483 <arnabmandal2912@gmail.com>
2025-12-26 09:41:22 +0530

    i like black

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import Archive from './pages/Archive';
 function App() {
   return (
     <Router>
-      <div className="bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900">
+      {/* Changed bg-slate-900 to bg-black */}
+      <div className="bg-black leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/archive" element={<Archive />} />
